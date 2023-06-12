6875c5d7739b57a23b444765f507e865438cf3a7
Pl4te <111096720+Pl4te@users.noreply.github.com>
2023-06-12 20:20:39 -0300

    Update conexao.php

--- viajei!/login/conexao.php
+++ viajei!/login/conexao.php
@@ -1,14 +1,19 @@
 <?php
 
-    $dbHost = 'ep-patient-shadow-166198-pooler.us-east-1.postgres.vercel-storage.com';
-    $dbUsername = 'default';
-    $dbPassword= 'vQrPNV9lheZ3';
-    $dbName = 'verceldb';
+    $dbHost = 'localhost';
+    $dbUsername = 'root';
+    $dbPassword= 'root';
+    $dbName = 'formulario_cadastro';
 
-    $conexao = pg_connect("host=$dbHost port=5432 dbname=$dbName user=$dbUsername password=$dbPassword") or die("Falha ao se conectar");
-    
-    return true;
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
 
-   
+    if($conexao->connect_errno) {
+
+        echo "Falha ao se conectar: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+   }
+   else
+   echo "conectado"
+
+
 
 ?>
